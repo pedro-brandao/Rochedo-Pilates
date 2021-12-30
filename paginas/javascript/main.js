e8252554a48a03ddaa1be0b9013aaa323540987b
pedro-brandao <pedrobrandao30.03@gmail.com>
2021-12-30 18:30:26 -0300

    first commit

---
+++ paginas/javascript/main.js
@@ -0,0 +1,58 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+//opem/close menu hamburguer
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//hidden/visble menu
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//shadow header
+function changeHeaderWhenscroll() {
+
+  const heaer = document.querySelector("#header")
+  const navHeight = header.offsetHeight
+
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+// testimonials carrossel
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination:{
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+
+})
+
+//back to top
+function backToTop(){
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 560){
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//Function When Scroll
+window.addEventListener('scroll', function(){
+  changeHeaderWhenscroll(),
+  backToTop()
+})
